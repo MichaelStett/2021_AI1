cb0de6f421bfb528284ef435bb3bd2542fc0f260
darpek123 <74000917+darpek123@users.noreply.github.com>
2021-01-11 17:21:50 +0100

    Add files via upload

---
+++ autoload.php
@@ -0,0 +1,31 @@
+<?php
+
+# Models
+require_once __DIR__ . '/models/currencyEnum.php';
+require_once __DIR__ . '/models/InvoiceSale.php';
+require_once __DIR__ . '/models/InvoicePurchase.php';
+require_once __DIR__ . '/models/User.php';
+require_once __DIR__ . '/models/repositories/UserRepository.php';
+require_once __DIR__ . '/models/repositories/InvoiceSaleRepository.php';
+require_once __DIR__ . '/models/repositories/InvoicePurchaseToDB.php';
+require_once __DIR__ . '/models/repositories/InvoiceSaleToDB.php';
+
+# Views
+require_once __DIR__ . '/views/Layout.php';
+require_once __DIR__ . '/views/login/LoginIndexView.php';
+require_once __DIR__ . '/views/invoiceSale/invoiceSaleIndexView.php';
+require_once __DIR__ . '/views/invoicePurchase/InvoicePurchaseFormView.php';
+require_once __DIR__ . '/views/invoiceSale/InvoiceSaleFormView.php';
+
+
+# Controllers 
+require_once __DIR__ . '/controllers/LoginController.php';
+require_once __DIR__ . '/controllers/InvoiceSaleController.php';
+require_once __DIR__ . '/controllers/InvoicePurchaseController.php';
+
+# Misc
+require_once __DIR__ . '/config.php';
+
+
+
+?>
